ebeab198018ba062f34c4bcef3229ec9ca3fd707
luis1987 <luis.padilla@sistemabea.mx>
2017-09-22 18:15:16 -0500

    SelfHostingThrougServiceReferenceDemo AND SelfHostingThroughConfigFileDemo

---
+++ SelfHostingThroughConfigFileDemo/SelfHost/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathServiceLibrary;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace SelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var serviceHost = new ServiceHost(typeof(MathService)))
+            {
+                /*ServiceEndpoint BasicHttpEndPoint1 = serviceHost.AddServiceEndpoint(
+                    typeof(IMathService),
+                    new BasicHttpBinding(),
+                    "http://127.0.0.1:4444/MathService"
+                );
+
+                ServiceEndpoint BasicHttpEndPoint2 = serviceHost.AddServiceEndpoint(
+                    typeof(IMathService),
+                    new BasicHttpBinding(),
+                    "http://127.0.0.1:5555/MathService"
+                );
+
+                ServiceEndpoint NettcpEndPoint = serviceHost.AddServiceEndpoint(
+                    typeof(IMathService),
+                    new NetTcpBinding(),
+                    "net.tcp://127.0.0.1:6666/IMathService"
+                );*/
+
+                serviceHost.Open();
+
+                Console.WriteLine("Your WCF service is running.");
+                Console.WriteLine(
+                    "Your WCF Math service is running and is listening on below endpoint:"
+                );
+                /*Console.WriteLine(
+                    "{0} ({1})",
+                    BasicHttpEndPoint1.Address.ToString(),
+                    BasicHttpEndPoint1.Binding.Name
+                );*/
+
+                foreach (ServiceEndpoint endpoint in serviceHost.Description.Endpoints)
+                {
+                    Console.WriteLine(
+                        "Address: {0} Binding Name: {1}",
+                        endpoint.Address.ToString(),
+                        endpoint.Binding.Name
+                    );
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Press any key to stop your WCF Math service.");
+                Console.ReadKey();
+
+                serviceHost.Close();
+            }
+        }
+    }
+}
---
+++ SelfHostingThroughConfigFileDemo/WindowsClient1/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MathServiceLibrary;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace WindowsClient1
+{
+    public partial class Form1 : Form
+    {
+        private IMathService basicHttpChannel = null;
+        private IMathService wsHttpChannel = null;
+        private IMathService nettcpChannel = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            basicHttpChannel = new ChannelFactory<IMathService>("MathService_BasicHttp")
+                .CreateChannel();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MyNumbers mynumbers = new MyNumbers()
+            {
+                Number1 = Convert.ToInt32(textBox1.Text),
+                Number2 = Convert.ToInt32(textBox2.Text)
+            };
+
+            textBox3.Text = basicHttpChannel.Add(mynumbers).ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MyNumbers mynumbers = new MyNumbers()
+            {
+                Number1 = Convert.ToInt32(textBox1.Text),
+                Number2 = Convert.ToInt32(textBox2.Text)
+            };
+
+            textBox3.Text = basicHttpChannel.Subtract(mynumbers).ToString();
+        }
+    }
+}
